1fa97dba2eeca161269ff7a3ff65431997d7631d
asha42 <asha.gupta421@gmail.com>
2022-08-05 00:15:37 +0530

    Create compare-the-triplets.php

---
+++ compare-the-triplets.php
@@ -0,0 +1,20 @@
+<?php
+
+//URL = https://www.hackerrank.com/challenges/compare-the-triplets
+ 
+function compareTriplets($a, $b) {
+    $alice_points = 0;
+    $bob_points = 0;
+   for($i = 0;$i<=count($a);$i++){
+       if($a[$i]>$b[$i]){
+           $alice_points++;
+       }else if ($a[$i]<$b[$i]){
+           $bob_points++;
+       }
+   }
+   
+   return [$alice_points, $bob_points];
+}
+ 
+print_r(compareTriplets([1, 2, 3], [3, 2, 1]));
+?>
