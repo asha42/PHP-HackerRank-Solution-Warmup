0b86fe98b50ee8d9cdafdc5e1717f810d5d60361
asha42 <asha.gupta421@gmail.com>
2022-08-05 15:36:18 +0530

    Plus Minus

---
+++ plus_minus.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus($arr) {
+    
+    for($i=0;$i<count($arr);$i++){
+        if($arr[$i]>0){
+            $round[0]++;   
+        }
+        else if($arr[$i]<0){
+            $round[1]++;   
+        }else{
+             $round[2]++;   
+        }
+    }
+    
+    $total = $round[0]+$round[1]+$round[2];
+    echo round($round[0]/$total,6)."\n";
+     echo round($round[1]/$total,6)."\n";
+      echo round($round[2]/$total,6)."\n";
+}
+plusMinus([-4, 3, -9, 0, 4, 1]);
+?>
