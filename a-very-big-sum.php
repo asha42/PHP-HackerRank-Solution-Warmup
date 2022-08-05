9c30256ff949cb758f3584591eebb49d8be58577
asha42 <asha.gupta421@gmail.com>
2022-08-05 15:23:50 +0530

    A Very Big Sum
    It must return the sum of all array elements.
    Sample Input
    1000000001 1000000002 1000000003 1000000004 1000000005
    Output
    5000000015

---
+++ a-very-big-sum.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum($ar) {
+    
+    return array_sum($ar);
+
+}
+echo aVeryBigSum([10,3,19,30]);
+?>
