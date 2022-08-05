7a6f8f40f0447db136dd30057cd3143d4c0b7bfa
asha42 <asha.gupta421@gmail.com>
2022-08-05 15:29:40 +0530

    Diagonal Difference
    Given a square matrix, calculate the absolute difference between the sums of its diagonals. 
    Sample Input
    3
    11 2 4
    4 5 6
    10 8 -12
    
    Sample Output
    
    15
    
    Explanation
    
    The primary diagonal is:
    
    11
       5
         -12
    
    Sum across the primary diagonal: 11 + 5 - 12 = 4

---
+++ diagonal-difference.php
@@ -0,0 +1,19 @@
+<?php
+// URL: https://www.hackerrank.com/challenges/diagonal-difference
+
+function diagonalDifference($arr) {
+  $left_diagnol = 0;
+  $right_diagnol = 0;
+  $totl_element = count($arr);
+  for($i=0;$i<=$totl_element;$i++){
+      $left_diagnol+=$arr[$i][$i];
+      $right_diagnol+=$arr[$i][$totl_element-$i-1];
+  }
+return abs($left_diagnol - $right_diagnol);
+}
+print_r(diagonalDifference([
+    [1, 2, 5], 
+    [4, 5, 6],
+    [7, 8, 12]
+]));
+?>
